15884b0734acf0442a10f95d5b14423279b02031
manish <manish.kumar@chainthat.com>
2023-07-30 13:49:22 +0530

    added initial commit. manish kumar

---
+++ ds-algo-brain/src/main/java/com/manish/dsalgobrain/DsAlgoBrainApplication.java
@@ -0,0 +1,13 @@
+package com.manish.dsalgobrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsAlgoBrainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsAlgoBrainApplication.class, args);
+	}
+
+}
